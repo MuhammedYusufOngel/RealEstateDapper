31645d2f06a38a85b12b8f9d0791eefdd1d77e53
Yusuf Öngel <Yusuf ngel@DESKTOP-KB4N4TR>
2024-08-18 03:37:03 +0300

    -Gerçekten artık bitti

--- RealEstateDapperUI/ViewComponents/EstateAgent/_EstateAgentNavbarSettingsComponentPartial.cs
+++ RealEstateDapperUI/ViewComponents/EstateAgent/_EstateAgentNavbarSettingsComponentPartial.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstateDapperUI.Dtos.ContactDtos;
+using RealEstateDapperUI.Dtos.EmployeeDtos;
+using RealEstateDapperUI.Services;
 
 namespace RealEstateDapperUI.ViewComponents.EstateAgent
 {
     public class _EstateAgentNavbarSettingsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILoginService _loginService;
+
+        public _EstateAgentNavbarSettingsComponentPartial(IHttpClientFactory httpClientFactory, ILoginService loginService)
         {
+            _httpClientFactory = httpClientFactory;
+            _loginService = loginService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userId = _loginService.GetUserId;
+
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7195/api/Login/{userId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultEmployeeWithAppUserDto>(jsonData);
+                ViewBag.image = values.ImageUrl;
+                ViewBag.name = values.UserName;
+            }
             return View();
         }
     }
